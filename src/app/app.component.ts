ea05fdc87fc814c0839b31fa88dfd0c0552375a5
Ian Ditsch <IanDitsch@Outlook.com>
2019-07-21 23:56:48 -0500

    Express.js Lesson 9 Planets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
+
+
 import { Component } from '@angular/core';
 
 @Component({
+  
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'planets';
+
+  title = 'Angular 2 Planets';
+  staticPath: string = 'http://localhost:3001/staticPlanets';
+  starTrekPath: string = 'http://localhost:3001/starTrekPlanets';
 }
+
